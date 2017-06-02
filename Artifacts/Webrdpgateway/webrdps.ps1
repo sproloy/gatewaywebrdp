6b124166f34186e774fe665cb1dbae055d4f7350
sproloy <proloy99@gmail.com>
2017-06-02 15:25:40 +0530

    changes

--- Artifacts/Webrdpgateway/webrdps.ps1
+++ Artifacts/Webrdpgateway/webrdps.ps1
@@ -23,25 +23,20 @@ function WaitForFile($File) {
 
 #Setup Folders
 
-$setupFolder = "c:\colaberry"
+$setupFolder = "c:\gatewaytohell"
 Create-Folder "$setupFolder"
 
 Create-Folder "$setupFolder\training"
 $setupFolder = "$setupFolder\training"
 
-Create-Folder "$setupFolder\sqlbi"
-Create-Folder "$setupFolder\sqlbi\datasets"
-Create-Folder "$setupFolder\sqlbi\installations"
-$setupFolder = "$setupFolder\sqlbi\installations"
-
 $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
 
 # SQL Server Installation 
-if((Test-Path "$setupFolder\SQLServer2016-SSEI-Dev.exe") -eq $false)
+if((Test-Path "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe") -eq $false)
 {
-    Write-Host "Downloading SQL Server installation file.."
+    Write-Host "Downloading Webrdp installation file.."
     if ($os_type -eq "True"){
-        Download-File "http://download.microsoft.com/download/4/4/F/44F2C687-BD92-4331-9D4F-882A5AB0D301/SQLServer2016-SSEI-Dev.exe" "$setupFolder\SQLServer2016-SSEI-Dev.exe"
+        Download-File "https://bedofrazors-my.sharepoint.com/personal/pro_bedofrazors_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=102e4dd8037054e788357fd9763af99d8&authkey=AVJjvuxk6eWahwBhcqxIHjM" "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe"
     }else {
         Write-Host "32 Bit system is not supported"
     }    
@@ -49,78 +44,20 @@ if((Test-Path "$setupFolder\SQLServer2016-SSEI-Dev.exe") -eq $false)
 
 # Prepare Configuration file
 Write-Host "Preparing configuration file.."
-if((Test-Path "$setupFolder\ConfigurationFile.ini") -eq $false)
+if((Test-Path "$setupFolder\webrdp.ini") -eq $false)
 {
-    Write-Host "Downloading SQL Server installation file.."
+    Write-Host "Downloading  installation file.."
     if ($os_type -eq "True"){
-        Download-File "https://raw.githubusercontent.com/Colaberry/training/master/sqlbi/installations/ConfigurationFile.ini" "$setupFolder\ConfigurationFile.ini"
+        Download-File "https://github.com/sproloy/gatewaywebrdp/blob/master/Artifacts/Webrdpgateway/webrdp.ini" "$setupFolder\webrdp.ini"
     }else {
         Write-Host "32 Bit system is not supported"
     }    
 }
 
-# SSMS Installation 
-if((Test-Path "$setupFolder\SSMS-Setup-ENU.exe") -eq $false)
-{
-    Write-Host "Downloading SSMS installation file.."
-    if ($os_type -eq "True"){
-        Download-File "https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe" "$setupFolder\SSMS-Setup-ENU.exe"
-    }else {
-        Write-Host "32 Bit system is not supported"
-    }    
-}
-
-# SSDT Installation 
-if((Test-Path "$setupFolder\SSDTSetup.exe") -eq $false)
-{
-    Write-Host "Downloading SSDT installation file.."
-    if ($os_type -eq "True"){
-        Download-File "https://download.microsoft.com/download/9/C/7/9C749FF7-7AD2-409A-BF75-69238295A668/Dev14/EN/SSDTSetup.exe" "$setupFolder\SSDTSetup.exe"
-    }else {
-        Write-Host "32 Bit system is not supported"
-    }    
-}
-
-# Download Adventureworks
-# AdventureWorks2012_Data.mdf
-# https://msftdbprodsamples.codeplex.com/downloads/get/165399
-if((Test-Path "$setupFolder\..\datasets\AdventureWorks2012_Data.mdf") -eq $false)
-{
-    Write-Host "Downloading Adventuresworks data file.."
-    if ($os_type -eq "True"){
-        Download-File "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=msftdbprodsamples&DownloadId=165399&FileTime=129762331847030000&Build=21031" "$setupFolder\..\datasets\AdventureWorks2012_Data.mdf"
-    }else {
-        Write-Host "32 Bit system is not supported"
-    }    
-}
-
-(Get-Content $setupFolder\ConfigurationFile.ini).replace('USERNAMETBR', "$env:computername\$env:username") | Set-Content $setupFolder\ConfigurationFile_local.ini
-
-Write-Host "Installing SQL Server.."
-Start-Process -FilePath "$setupFolder\SQLServer2016-SSEI-Dev.exe" -ArgumentList '/ConfigurationFile="c:\colaberry\training\sqlbi\installations\ConfigurationFile_local.ini"', '/MediaPath="c:\colaberry\training\sqlbi\installations"', '/IAcceptSqlServerLicenseTerms', '/ENU', '/QS'  -Wait
 
 
-Write-Host "Installing SSMS.."
-Start-Process -FilePath "$setupFolder\SSMS-Setup-ENU.exe" -ArgumentList '/install','/passive' -Wait
-
-Write-Host "Installing SSDT.."
-Start-Process -FilePath "$setupFolder\SSDTSetup.exe" -ArgumentList '/INSTALLALL=1', '/passive', '/promptrestart' -Wait
-
-Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue   
-Import-Module SQLPS -WarningAction SilentlyContinue  
-
-$AttachCmd = @"  
-USE [master]  CREATE DATABASE [AdventureWorks2012] ON (FILENAME ='$setupFolder\..\datasets\AdventureWorks2012_Data.mdf') for ATTACH  
-"@  
-Invoke-Sqlcmd $attachCmd -QueryTimeout 3600 -ServerInstance $env:computername\CB2016SQLSERVER 
-If($?)  
-{  
-	Write-Host 'Attached database sucessfully!'  
-}  
-else  
-{  
-	Write-Host 'Attaching Failed!'  
-}
+Write-Host "Installing.."
+Start-Process -FilePath "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe" -ArgumentList '/ConfigurationFile="c:\gatewaytohell\training\webrdp.ini"'
 
 
 Write-Host 'Installation completed.'
