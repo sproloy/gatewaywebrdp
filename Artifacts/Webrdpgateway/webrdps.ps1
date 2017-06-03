2ae9542c5606dc64a58a0093153b016bbe06022a
sproloy <proloy99@gmail.com>
2017-06-03 18:14:58 +0530

    changes

--- Artifacts/Webrdpgateway/webrdps.ps1
+++ Artifacts/Webrdpgateway/webrdps.ps1
@@ -31,12 +31,22 @@ $setupFolder = "$setupFolder\training"
 
 $os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
 
-# SQL Server Installation 
+# Web rdp gateway Installation 
 if((Test-Path "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe") -eq $false)
 {
     Write-Host "Downloading Webrdp installation file.."
     if ($os_type -eq "True"){
-        Download-File "https://bedofrazors-my.sharepoint.com/personal/pro_bedofrazors_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=102e4dd8037054e788357fd9763af99d8&authkey=AVJjvuxk6eWahwBhcqxIHjM" "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe"
+        Download-File "" "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe"
+    }else {
+        Write-Host "32 Bit system is not supported"
+    }    
+}
+# download webrdp auto it exe
+if((Test-Path "$setupFolder\webrdpgatewayscript.exe") -eq $false)
+{
+    Write-Host "Downloading Webrdp installation file.."
+    if ($os_type -eq "True"){
+        Download-File "https://cradle123-my.sharepoint.com/personal/cradleoffilth_cradle123_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=139d79315e4ec421fbfcbff3d6336fca3&authkey=AQHezZo53le0h4cKlUKXkpc" "$setupFolder\webrdpgatewayscript.exe"
     }else {
         Write-Host "32 Bit system is not supported"
     }    
@@ -47,27 +57,36 @@ if((Test-Path "$setupFolder\G7_CR_Technologies-license.swl") -eq $false)
 {
     Write-Host "Downloading Webrdp license file.."
     if ($os_type -eq "True"){
-        Download-File "https://bedofrazors-my.sharepoint.com/personal/pro_bedofrazors_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=18ca37386b0f9422ba607060fdf7d8d26&authkey=AUXZxSkXPGlf_dK-w-vYogo" "$setupFolder\G7_CR_Technologies-license.swl"
+        Download-File "https://cradle123-my.sharepoint.com/personal/cradleoffilth_cradle123_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=14f7747c19fc34c5ea0a50e496499e273&authkey=ATTn7AjZ-r-ANVMmGR1Klrk" "$setupFolder\G7_CR_Technologies-license.swl"
     }else {
         Write-Host "32 Bit system is not supported"
     }    
 }
-# Prepare Configuration file
-Write-Host "Preparing configuration file.."
-if((Test-Path "$setupFolder\webrdp.ini") -eq $false)
+# download background image
+Write-Host "Downloading background image.."
+if((Test-Path "$setupFolder\M4LCircle.png") -eq $false)
 {
-    Write-Host "Downloading  installation file.."
+    Write-Host "Downloading  background file.."
     if ($os_type -eq "True"){
-        Download-File "https://github.com/sproloy/gatewaywebrdp/blob/master/Artifacts/Webrdpgateway/webrdp.ini" "$setupFolder\webrdp.ini"
+        Download-File "https://cradle123-my.sharepoint.com/personal/cradleoffilth_cradle123_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=17da86e35929a429fbebcd660a8dbde2b&authkey=AfbvufEaHLtTSz313DDGotM" "$setupFolder\M4LCircle.png"
+    }else {
+        Write-Host "32 Bit system is not supported"
+    }    
+}
+# download logo
+Write-Host "Downloading logo.."
+if((Test-Path "$setupFolder\Logo_Part1_White_100_45.png") -eq $false)
+{
+    Write-Host "Downloading logo.."
+    if ($os_type -eq "True"){
+        Download-File "https://cradle123-my.sharepoint.com/personal/cradleoffilth_cradle123_onmicrosoft_com/_layouts/15/guestaccess.aspx?docid=16cdb427de8c549798a3f0f02d99c91f0&authkey=AdXF9-aIOMMGHxgzki4XHLQ" "$setupFolder\Logo_Part1_White_100_45.png"
     }else {
         Write-Host "32 Bit system is not supported"
     }    
 }
 
 
-
-Write-Host "Installing.."
-Start-Process -FilePath "$setupFolder\webRDP-Gateway_1.2.0.43-Win-64.exe" -ArgumentList '/ConfigurationFile="c:\gatewaytohell\training\webrdp.ini"'
-
+Write-Host "Installing webrdp.."
+Start-Process -FilePath "$setupFolder\webrdpgatewayscript.exe"
 
 Write-Host 'Installation completed.'
