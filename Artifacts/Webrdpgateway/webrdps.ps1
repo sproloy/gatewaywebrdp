546811eafde6101b75ea5fe1aca362a524ce2c8e
sproloy <proloy99@gmail.com>
2017-06-06 14:36:37 +0530

    changes

--- Artifacts/Webrdpgateway/webrdps.ps1
+++ Artifacts/Webrdpgateway/webrdps.ps1
@@ -85,5 +85,6 @@ if((Test-Path "$setupFolder\Logo_Part1_White_100_45.png") -eq $false)
     }    
 }
 Write-Host "Installing webrdp.."
-Start-Process -FilePath "C:\gatewaytohell\training\webrdpscript" -Wait
-Write-Host 'Installation completed.'
+$proc = (Start-Process -FilePath  "C:\gatewaytohell\training\webrdpscript.exe")
+$ExitCode = $proc.ExitCode
+Exit($ExitCode)
